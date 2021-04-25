89a4c69f77e34e4a0a7350407772985dabd9a010
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-25 17:05:49 -0300

    injeção dependência acesso ao banco

--- src/pessoas/pessoas.module.ts
+++ src/pessoas/pessoas.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { PessoasController } from './pessoas.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pessoa, PessoaSchema } from './entities/pessoa.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Pessoa.name, schema: PessoaSchema }]),
+  ],
   controllers: [PessoasController],
-  providers: [PessoasService]
+  providers: [PessoasService],
 })
 export class PessoasModule {}
