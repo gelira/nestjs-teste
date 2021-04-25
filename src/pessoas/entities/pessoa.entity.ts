0f57bf34bdeaab062f7dde671d209cad2c94722e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-25 17:04:15 -0300

    schema mongo

--- src/pessoas/entities/pessoa.entity.ts
+++ src/pessoas/entities/pessoa.entity.ts
@@ -1,1 +1,32 @@
-export class Pessoa {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+  collection: 'pessoas',
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = doc.id;
+      delete ret._id;
+      return ret;
+    },
+  },
+  versionKey: false,
+})
+export class Pessoa {
+  @Prop()
+  nome: string;
+
+  @Prop()
+  idade: number;
+
+  @Prop()
+  sexo: string;
+}
+
+export type PessoaDocument = Pessoa & Document;
+
+export const PessoaSchema = SchemaFactory.createForClass(Pessoa);
