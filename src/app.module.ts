31d370b94a2d83924fa42a0f37f0b315db67be40
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-25 14:04:18 -0300

    conexão com mondogb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PessoasModule } from './pessoas/pessoas.module';
 
 @Module({
-  imports: [PessoasModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27018/nest'),
+    PessoasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
