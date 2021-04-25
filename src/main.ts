e76b29abd80a3a35a5239ad7909f5d9bcf8586d7
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-25 17:03:26 -0300

    definindo validação pipe

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      transform: true,
     }),
   );
-
   await app.listen(3000);
 }
 bootstrap();
