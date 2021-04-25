51009a3d67dd95a66af7773b7e3996a0f18e0518
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-25 15:14:47 -0300

    validação DTO

--- src/pessoas/dto/create-pessoa.dto.ts
+++ src/pessoas/dto/create-pessoa.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePessoaDto {}
+import { IsString, IsInt, Min, IsIn } from 'class-validator';
+
+export class CreatePessoaDto {
+  @IsString()
+  nome: string;
+
+  @IsInt()
+  @Min(0)
+  idade: number;
+
+  @IsString()
+  @IsIn(['M', 'F'])
+  sexo: string;
+}
