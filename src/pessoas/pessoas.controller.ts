22610265edef7fa5e638c12b4f8f7c7763e925a8
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-25 17:07:24 -0300

    CRUD Pessoa

--- src/pessoas/pessoas.controller.ts
+++ src/pessoas/pessoas.controller.ts
@@ -1,11 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotFoundException,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { ParseObjectIdPipe } from '../pipes/mongodb-objectid.pipe';
 
 @Controller('pessoas')
 export class PessoasController {
   constructor(private readonly pessoasService: PessoasService) {}
+
+  checkNotFound(pessoa: any) {
+    if (!pessoa) {
+      throw new NotFoundException();
+    }
+  }
 
   @Post()
   create(@Body() createPessoaDto: CreatePessoaDto) {
@@ -18,17 +36,26 @@ export class PessoasController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pessoasService.findOne(+id);
+  async findOne(@Param('id', ParseObjectIdPipe) id: string) {
+    const pessoa = await this.pessoasService.findOne(id);
+    this.checkNotFound(pessoa);
+    return pessoa;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePessoaDto: UpdatePessoaDto) {
-    return this.pessoasService.update(+id, updatePessoaDto);
+  async update(
+    @Param('id', ParseObjectIdPipe) id: string,
+    @Body() updatePessoaDto: UpdatePessoaDto,
+  ) {
+    const pessoa = await this.pessoasService.update(id, updatePessoaDto);
+    this.checkNotFound(pessoa);
+    return pessoa;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pessoasService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', ParseObjectIdPipe) id: string) {
+    const pessoa = await this.pessoasService.remove(id);
+    this.checkNotFound(pessoa);
   }
 }
--- src/pessoas/pessoas.service.ts
+++ src/pessoas/pessoas.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { Pessoa, PessoaDocument } from './entities/pessoa.entity';
 
 @Injectable()
 export class PessoasService {
-  create(createPessoaDto: CreatePessoaDto) {
-    return 'This action adds a new pessoa';
+  constructor(
+    @InjectModel(Pessoa.name) private pessoaModel: Model<PessoaDocument>,
+  ) {}
+
+  async create(createPessoaDto: CreatePessoaDto) {
+    const pessoa = new this.pessoaModel(createPessoaDto);
+    return pessoa.save();
   }
 
   findAll() {
-    return `This action returns all pessoas`;
+    return this.pessoaModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pessoa`;
+  findOne(id: string) {
+    return this.pessoaModel.findById(id).exec();
   }
 
-  update(id: number, updatePessoaDto: UpdatePessoaDto) {
-    return `This action updates a #${id} pessoa`;
+  async update(id: string, updatePessoaDto: UpdatePessoaDto) {
+    const filter = { _id: id };
+    const { n } = await this.pessoaModel.updateOne(filter, updatePessoaDto);
+    if (n === 0) {
+      return null;
+    }
+    return this.pessoaModel.findById(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pessoa`;
+  async remove(id: string) {
+    const pessoa = await this.pessoaModel.findById(id);
+    if (!pessoa) {
+      return null;
+    }
+    return pessoa.remove();
   }
 }
