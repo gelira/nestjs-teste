a369b87e3a5f50488ce6989b69afefae3ebe86e1
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-30 18:52:43 -0300

    conexão User

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+  collection: 'users',
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = doc.id;
+      delete ret._id;
+      return ret;
+    },
+  },
+  versionKey: false,
+})
+export class User {
+  @Prop()
+  nome: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  senha: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
