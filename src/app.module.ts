7da7b178b3c403bc7dc1f1385cda4a2261a1fb39
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-30 18:39:01 -0300

    módulo auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PessoasModule } from './pessoas/pessoas.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
       }),
     }),
     PessoasModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
