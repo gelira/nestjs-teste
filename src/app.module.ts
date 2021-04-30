090292ca4ffe4a5b28cb7e135a398b868d3bcd8f
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-30 09:06:51 -0300

    configurando banco de dados com .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,7 +15,12 @@ import { PessoasModule } from './pessoas/pessoas.module';
       isGlobal: true,
       cache: true,
     }),
-    MongooseModule.forRoot('mongodb://localhost:27018/nest'),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_CONNECTION'),
+      }),
+    }),
     PessoasModule,
   ],
   controllers: [AppController],
