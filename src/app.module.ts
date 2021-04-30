d5aad42d721667ded9109f4efe44207654ca7916
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-30 08:56:40 -0300

    módulo de condiguração

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +7,14 @@ import { PessoasModule } from './pessoas/pessoas.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+      isGlobal: true,
+      cache: true,
+    }),
     MongooseModule.forRoot('mongodb://localhost:27018/nest'),
     PessoasModule,
   ],
