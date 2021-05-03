93ada035830eb11e2df5678f32eab0fd519817c9
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-05-03 08:02:41 -0300

    chamando microsserviço a partir de controller HTTP

--- src/pessoas/controllers/pessoas.controller.ts
+++ src/pessoas/controllers/pessoas.controller.ts
@@ -10,17 +10,23 @@ import {
   HttpCode,
   HttpStatus,
   UseGuards,
+  Inject,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { PessoasService } from '../services/pessoas.service';
 import { CreatePessoaDto } from '../dto/create-pessoa.dto';
 import { UpdatePessoaDto } from '../dto/update-pessoa.dto';
 import { ParseObjectIdPipe } from '../../pipes/mongodb-objectid.pipe';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('pessoas')
 @UseGuards(JwtAuthGuard)
 export class PessoasController {
-  constructor(private readonly pessoasService: PessoasService) {}
+  constructor(
+    private readonly pessoasService: PessoasService,
+    @Inject('TESTE_SERVICE') private client: ClientProxy,
+  ) {}
 
   checkNotFound(pessoa: any) {
     if (!pessoa) {
@@ -36,6 +42,15 @@ export class PessoasController {
   @Get()
   findAll() {
     return this.pessoasService.findAll();
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Get('teste/:n')
+  async teste(@Param('n', ParseIntPipe) n: number) {
+    const moment = new Date();
+    moment.setSeconds(moment.getSeconds() + 10);
+
+    this.client.emit('quadrado', { time: moment.getTime(), numero: n });
   }
 
   @Get(':id')
--- src/pessoas/pessoas.module.ts
+++ src/pessoas/pessoas.module.ts
@@ -4,11 +4,13 @@ import { PessoasController } from './controllers/pessoas.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pessoa, PessoaSchema } from './entities/pessoa.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { TesteModule } from 'src/teste/teste.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Pessoa.name, schema: PessoaSchema }]),
     AuthModule,
+    TesteModule,
   ],
   controllers: [PessoasController],
   providers: [PessoasService],
