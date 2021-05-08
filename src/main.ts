84764558a9b4b61dc566a007a1bd3c08e7cafa24
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-05-08 15:47:37 -0300

    extraindo redis adapter

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { RedisIoAdapter } from './modules/pessoas/gateways/redis.adapter';
+import { RedisIoAdapter } from './ws-adapters/redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
