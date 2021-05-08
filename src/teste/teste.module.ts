8f82ab49ddf8a965ea17c4d27d9db8c8196aa613
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-05-08 15:28:59 -0300

    removendo importação ClientModule

--- src/teste/teste.module.ts
+++ src/teste/teste.module.ts
@@ -1,31 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-  ClientProxyFactory,
-  ClientsModule,
-  Transport,
-} from '@nestjs/microservices';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import { TesteController } from './teste.controller';
 
 @Module({
-  imports: [
-    ClientsModule.registerAsync([
-      {
-        name: 'TESTE_SERVICE',
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [configService.get<string>('RABBITMQ_CONNECTION')],
-            queue: configService.get<string>('RABBITMQ_QUEUE'),
-            noAck: false,
-          },
-        }),
-      },
-    ]),
-    forwardRef(() => PessoasModule),
-  ],
+  imports: [forwardRef(() => PessoasModule)],
   controllers: [TesteController],
   providers: [
     {
